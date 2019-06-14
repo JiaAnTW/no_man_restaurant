871218b9cc999cfd56cdfeaeddccadbc91efc567
JiaAnTW <39335864+JiaAnTW@users.noreply.github.com>
2019-06-14 15:21:32 +0800

    Reupload backend to fix some mistake during uisng git.

--- server/controllers/feedbackController.js
+++ server/controllers/feedbackController.js
@@ -5,7 +5,10 @@ module.exports=class feedbackController{
             Menu=new Menu();
             //console.log(req)
             var data=Menu.GetFeedback(req.body.id)
-            res.send(data);
+            data.then((value)=>{
+                res.send(value);
+            })
+            
         }catch(error){
             console.log("FeedbackController/Feedback error: "+error);
         }
@@ -15,7 +18,7 @@ module.exports=class feedbackController{
         try{
             var Menu = require( '../models/menu.js');
             Menu=new Menu();
-            Menu.Comment(req.body.id,req.body.content);
+            Menu.Comment(req.body.id,req.body.score,req.body.content);
         }catch(error){
             console.log("FeedbackController/Comment error: "+error);
         }
